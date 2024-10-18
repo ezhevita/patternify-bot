e705202f737e45b6c03972d2e466ecba2acd13e2
Vita Chumakova <me@ezhevita.dev>
2024-10-18 20:45:03 +0400

    perf: process input as utf8 chars

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use confusables::Confusable;
-use encode_unicode::StrExt;
 use teloxide::{
+    payloads::SendMessage,
     prelude::*,
+    requests::JsonRequest,
     types::{
         InlineQueryResult, InlineQueryResultArticle, InputMessageContent, InputMessageContentText,
         MessageEntity, ReplyParameters,
@@ -100,27 +101,19 @@ async fn process_inline_query(bot: Bot, query: InlineQuery) -> ResponseResult<()
 }
 
 async fn process_reply(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
-    let reply_message = match msg.reply_to_message() {
-        Some(message) => message,
-        None => return Ok(()),
-    };
-
+    let reply_message = msg.reply_to_message().unwrap();
     let text_to_process = reply_message.text().or(reply_message.caption()).unwrap();
 
-    let mut entities_to_send = match (reply_message.entities(), reply_message.caption_entities()) {
-        (Some(entities), _) => entities,
-        (None, Some(entities)) => entities,
-        _ => return Ok(()),
-    }
-    .to_vec();
+    let mut entities_to_send = reply_message
+        .entities()
+        .or(reply_message.caption_entities())
+        .map_or_else(|| Vec::new(), |x| x.to_vec());
 
     let result = match cmd {
         Command::Patternify(pattern) => spoilerify(text_to_process, &pattern),
     };
 
-    if result.is_empty() {
-        return Ok(());
-    }
+    assert!(!result.is_empty());
 
     entities_to_send.extend(result.into_iter());
 
@@ -138,31 +131,39 @@ async fn process_reply(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<(
 }
 
 async fn process_message(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
-    let (result, input) = match cmd {
-        Command::Patternify(text) => match text.split_once(' ') {
-            Some((pattern, input)) => (spoilerify(&input, &pattern), input.to_string()),
-            None => return Ok(()),
-        },
-    };
-
-    if result.is_empty() {
-        return Ok(());
-    }
-
-    let mut entities = msg.entities().map_or_else(|| Vec::new(), |x| x.to_vec());
-    entities.extend(result.into_iter());
-
-    let response = bot
-        .send_message(msg.chat.id, input)
-        .entities(entities)
-        .reply_parameters(ReplyParameters::new(msg.id))
-        .await;
+    let response = _process_message(bot, msg, cmd).send().await;
 
     if let Err(err) = response {
         log::error!("Error in handler: {:?}", err);
     }
 
-    Ok(())
+    return Ok(());
+}
+
+fn _process_message(bot: Bot, msg: Message, cmd: Command) -> JsonRequest<SendMessage> {
+    let response = bot
+        .send_message(msg.chat.id, "")
+        .reply_parameters(ReplyParameters::new(msg.id));
+
+    let (result, input) = match cmd {
+        Command::Patternify(text) => match text.split_once(' ') {
+            Some((pattern, input)) if !pattern.is_empty() && !input.is_empty() => {
+                (spoilerify(&input, &pattern), input.to_string())
+            }
+            _ => {
+                return response
+                    .text("Invalid command usage, format: `/patternify <pattern without spaces> <input>`")
+                    .parse_mode(teloxide::types::ParseMode::MarkdownV2);
+            }
+        },
+    };
+
+    assert!(!result.is_empty());
+
+    let mut entities = msg.entities().map_or_else(|| Vec::new(), |x| x.to_vec());
+    entities.extend(result.into_iter());
+
+    response.text(input).entities(entities)
 }
 
 fn spoilerify(input: &str, pattern: &str) -> Vec<MessageEntity> {
@@ -179,48 +180,52 @@ fn spoilerify(input: &str, pattern: &str) -> Vec<MessageEntity> {
     );
 
     let mut pattern_indexes = Vec::with_capacity(pattern.chars().count());
-
     let mut entities = Vec::new();
+    let mut input_iterator = input.chars();
+    let mut u16_index = 0;
+
     let lowercased_pattern = pattern.to_lowercase();
 
-    let mut previous_index = 0;
     loop {
-        let started_from_ind = previous_index;
+        let started_from_ind = u16_index;
         for pattern_char in lowercased_pattern.chars() {
             let pattern_char_str = pattern_char.to_string();
-            let pattern_char_conf = pattern_char_str.detect_replace_confusable();
+            let pattern_char_str_conf = pattern_char_str.detect_replace_confusable();
             let upper_pattern_char_str = pattern_char.to_uppercase().to_string();
             let upper_pattern_char_str_conf = upper_pattern_char_str.detect_replace_confusable();
+
             let mut value_found = false;
-            let mut u16_index = previous_index;
-            for input_char in input.utf16chars().skip(previous_index) {
+
+            loop {
+                let input_char = match input_iterator.next() {
+                    Some(x) => x,
+                    None => break,
+                };
+
                 let input_char_str = input_char.to_string();
                 let input_char_str_conf = input_char_str.detect_replace_confusable();
-                if input_char_str_conf == pattern_char_conf
+                let current_u16_index = u16_index;
+                u16_index += input_char.len_utf16();
+
+                if input_char_str_conf == pattern_char_str_conf
                     || input_char_str_conf == upper_pattern_char_str_conf
                 {
-                    pattern_indexes.push(u16_index);
-                    previous_index = u16_index + 1;
+                    pattern_indexes.push(current_u16_index);
                     value_found = true;
                     break;
                 }
-
-                u16_index += 1;
             }
 
             if !value_found {
                 let last_ind = started_from_ind;
-                entities.push(MessageEntity::spoiler(
-                    last_ind,
-                    input.utf16chars().map(|x| x.len()).sum::<usize>() - last_ind,
-                ));
+                entities.push(MessageEntity::spoiler(last_ind, u16_index - last_ind));
 
                 return entities;
             }
         }
 
         let mut previous_offset = started_from_ind;
-        for i in pattern_indexes.iter().map(|x| *x) {
+        for i in pattern_indexes.iter().map(|x| x.clone()) {
             let length = i - previous_offset;
             if length > 0 {
                 entities.push(MessageEntity::spoiler(previous_offset, i - previous_offset));
